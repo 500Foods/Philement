a845badc0174f7378805daed8fc6224ac6a62ee6
Andrew Simard <41052272+500Foods@users.noreply.github.com>
2024-07-10 11:42:08 -0700

    Add files via upload

--- elements/001-hydrogen/hydrogen/src/logging.c
+++ elements/001-hydrogen/hydrogen/src/logging.c
@@ -6,9 +6,15 @@
 #include "configuration.h"
 #include "queue.h"
 
-void log_this(const char* subsystem, const char* details, int priority, bool LogConsole, bool LogDatabase, bool LogFile) {
+void log_this(const char* subsystem, const char* format, int priority, bool LogConsole, bool LogDatabase, bool LogFile, ...) {
+    char details[1024];  // Increased buffer size to accommodate formatted string
+    va_list args;
+    va_start(args, LogFile);
+    vsnprintf(details, sizeof(details), format, args);
+    va_end(args);
+
     // Create a JSON string with the log message details
-    char json_message[1024];
+    char json_message[2048];  // Increased buffer size
     snprintf(json_message, sizeof(json_message),
              "{\"subsystem\":\"%s\",\"details\":\"%s\",\"priority\":%d,\"LogConsole\":%s,\"LogDatabase\":%s,\"LogFile\":%s}",
              subsystem, details, priority, LogConsole ? "true" : "false", LogDatabase ? "true" : "false", LogFile ? "true" : "false");
@@ -21,4 +27,3 @@ void log_this(const char* subsystem, const char* details, int priority, bool Log
         fprintf(stderr, "Error: SystemLog queue not found\n");
     }
 }
-
