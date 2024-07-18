e4a7af5864f7c12d35b78cc1d95d4e297df2e52f
500Foods <andrew@500foods.com>
2024-07-17 17:00:44 -0700

    Lithium TMS WEB Core project created

---
+++ elements/003-lithium/UnitMain.pas
@@ -0,0 +1,25 @@
+unit UnitMain;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, JS, Web, WEBLib.Graphics, WEBLib.Controls,
+  WEBLib.Forms, WEBLib.Dialogs, Vcl.Controls, Vcl.StdCtrls, WEBLib.StdCtrls;
+
+type
+  TForm1 = class(TWebForm)
+    WebLabel1: TWebLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
