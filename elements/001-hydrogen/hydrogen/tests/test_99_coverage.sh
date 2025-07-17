7e236953f750a3bffc5628191d26d35d6459a4fa
Andrew Simard <andrew@500foods.com>
2025-07-16 17:26:46 -0700

    Documentation updates

--- elements/001-hydrogen/hydrogen/tests/test_99_coverage.sh
+++ elements/001-hydrogen/hydrogen/tests/test_99_coverage.sh
@@ -8,7 +8,7 @@
 # 2.0.0 - Initial version with comprehensive coverage analysis
 
 # Test configuration
-TEST_NAME="Test Suite Coverage (coverage_tables)"
+TEST_NAME="Test Suite Coverage (coverage_table)"
 SCRIPT_VERSION="2.0.1"
 
 # Get the directory where this script is located
