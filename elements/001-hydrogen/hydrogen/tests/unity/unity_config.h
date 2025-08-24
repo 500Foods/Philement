a32a56baab0a2c08982f38b9200657179430e1c4
Andrew Simard <andrew@500foods.com>
2025-08-24 14:45:55 -0700

    Lots of work on adding tests for beryllium and cleaning up output of beryllium. Fixing errors with test concurrency. And more.

---
+++ elements/001-hydrogen/hydrogen/tests/unity/unity_config.h
@@ -0,0 +1,11 @@
+/* Unity Configuration for Hydrogen Project
+ * This file enables double precision support for Unity tests
+ */
+
+#ifndef UNITY_CONFIG_H
+#define UNITY_CONFIG_H
+
+// Enable double precision support
+#define UNITY_INCLUDE_DOUBLE
+
+#endif /* UNITY_CONFIG_H */
