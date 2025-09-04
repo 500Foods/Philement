2008e17b098ef7398190ffc549be1dcb7dec7b22
Andrew Simard <andrew@500foods.com>
2025-09-04 10:55:05 -0700

    Cleanup of database test scripts

--- elements/001-hydrogen/hydrogen/tests/artifacts/database/db2/db2_test.sh
+++ elements/001-hydrogen/hydrogen/tests/artifacts/database/db2/db2_test.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+
+# This script runs through a check of the postgres database
+# - Create database 'hydrogen'
+# - Create table 'test456'
+# - Insert value '789'
+# - Select data from table
+# - Drop table
+# - Drop database
+
+set -eou pipefail
+
 db2 -tvf db2_test.sql
