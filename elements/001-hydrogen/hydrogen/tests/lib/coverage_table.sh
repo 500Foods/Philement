3fd51d7974fbf459ff33e4d39b75f4c0adc02eac
Andrew Simard <andrew@500foods.com>
2025-09-19 22:52:40 -0700

    More payload tests, tweak coverage to stop checking at 70% instead of 75%

--- elements/001-hydrogen/hydrogen/tests/lib/coverage_table.sh
+++ elements/001-hydrogen/hydrogen/tests/lib/coverage_table.sh
@@ -487,7 +487,7 @@ for file_data_entry in "${sorted_file_data[@]}"; do
 
             # Set focus to 0 if basic requirements are met
             if (comb_pct >= 50 && lines <= 100) focus = 0;
-            if (comb_pct >= 75 && lines >= 100) focus = 0;
+            if (comb_pct >= 70 && lines >= 100) focus = 0;
 
             printf "%.3f %.3f %.3f %.3f %d %.1f\n", u_pct, c_pct, comb_pct, max_pct, below_50, focus;
         }')"
