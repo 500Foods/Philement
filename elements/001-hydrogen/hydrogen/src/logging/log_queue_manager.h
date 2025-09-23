0308d8a474bc0208dff3dbd85ce357d2be9dda74
Andrew Simard <andrew@500foods.com>
2025-09-22 19:49:27 -0700

    More log_queue_manager.c tests

--- elements/001-hydrogen/hydrogen/src/logging/log_queue_manager.h
+++ elements/001-hydrogen/hydrogen/src/logging/log_queue_manager.h
@@ -12,5 +12,7 @@ bool should_log_to_console(const char* subsystem, int priority, const LoggingCon
 bool should_log_to_file(const char* subsystem, int priority, const LoggingConfig* config);
 bool should_log_to_database(const char* subsystem, int priority, const LoggingConfig* config);
 bool should_log_to_notify(const char* subsystem, int priority, const LoggingConfig* config);
+void process_log_message(const char* message, int priority);
+void cleanup_log_queue_manager(void* arg);
 
 #endif // LOG_QUEUE_MANAGER_H
