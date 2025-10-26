b6187b1573573e9eb8057bd84fc1cbb2f6e62303
Andrew Simard <andrew@500foods.com>
2025-10-26 10:56:32 -0700

    More enhancements to Lua migration scripts, upgraded 1005

--- elements/002-helium/acuranzo/migrations/acuranzo_1004.lua
+++ elements/002-helium/acuranzo/migrations/acuranzo_1004.lua
@@ -1,13 +1,13 @@
 -- Migration: acuranzo_1004.lua
 -- Creates the account_roles table and populating it with the next migration.
 
--- CHANGELOG
--- 2.0.0 - 2025-10-25 - Moved to latest migration format
--- 1.1.0 -0 2025-09-28 - Changed diagram query to use JSON table definition instead of PlantUML for custom ERD tool.
--- 1.0.0 -0 2025-09-13 - Initial creation for account_roles table with PostgreSQL support.
-
 -- luacheck: no max line length
 -- luacheck: no unused args
+
+-- CHANGELOG
+-- 2.0.0 - 2025-10-25 - Moved to latest migration format
+-- 1.1.0 - 2025-09-28 - Changed diagram query to use JSON table definition instead of PlantUML for custom ERD tool.
+-- 1.0.0 - 2025-09-13 - Initial creation for account_roles table with PostgreSQL support.
 
 return function(engine, design_name, schema_name, cfg)
 local queries = {}
@@ -35,12 +35,7 @@ table.insert(queries,{sql=[[
                 name                    ${VARCHAR_100}              ,
                 summary                 ${TEXTBIG}                  ,
                 collection              ${JSON}                     ,
-                valid_after             ${TIMESTAMP_TZ}             ,
-                valid_until             ${TIMESTAMP_TZ}             ,
-                created_id              ${INTEGER}          NOT NULL,
-                created_at              ${TIMESTAMP_TZ}     NOT NULL,
-                updated_id              ${INTEGER}          NOT NULL,
-                updated_at              ${TIMESTAMP_TZ}     NOT NULL,
+                ${COMMON_CREATE}
                 PRIMARY KEY(account_id, role_id, system_id)
             );
         ]=],
@@ -53,7 +48,7 @@ table.insert(queries,{sql=[[
         ]=],
                                                                             -- summary
         NULL,                                                               -- collection
-        ${QUERIES_COMMON}
+        ${COMMON_INSERT}
     );
 
 ]]})
@@ -84,7 +79,7 @@ table.insert(queries,{sql=[[
         ]=],
                                                                             -- summary
         NULL,                                                               -- collection
-        ${QUERIES_COMMON}
+        ${COMMON_INSERT}
     );
 
 ]]})
@@ -173,54 +168,7 @@ table.insert(queries,{sql=[[
                                 "unique": false,
                                 "standard": true
                             },
-                            {
-                                "name": "valid_after",
-                                "datatype": "${TIMESTAMP_TZ}",
-                                "nullable": true,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            },
-                            {
-                                "name": "valid_until",
-                                "datatype": "${TIMESTAMP_TZ}",
-                                "nullable": true,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            },
-                            {
-                                "name": "created_id",
-                                "datatype": "${INTEGER}",
-                                "nullable": false,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            },
-                            {
-                                "name": "created_at",
-                                "datatype": "${TIMESTAMP_TZ}",
-                                "nullable": false,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            },
-                            {
-                                "name": "updated_id",
-                                "datatype": "${INTEGER}",
-                                "nullable": false,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            },
-                            {
-                                "name": "updated_at",
-                                "datatype": "${TIMESTAMP_TZ}",
-                                "nullable": false,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            }
+                            ${COMMON_DIAGRAM}
                         ]
                     }
                 ]
@@ -229,7 +177,7 @@ table.insert(queries,{sql=[[
         ${JSON_INGEST_END}
                                                                             -- DIAGRAM_END
         ,                                                                   -- collection
-        ${QUERIES_COMMON}
+        ${COMMON_INSERT}
     );
 
 ]]})
