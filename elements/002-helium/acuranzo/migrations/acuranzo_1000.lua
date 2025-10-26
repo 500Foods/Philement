fd9821b18f2eb05fb679a1b092f71205b41ed41d
Andrew Simard <andrew@500foods.com>
2025-10-26 10:56:32 -0700

    More enhancements to Lua migration scripts, upgraded 1005

--- elements/002-helium/acuranzo/migrations/acuranzo_1000.lua
+++ elements/002-helium/acuranzo/migrations/acuranzo_1000.lua
@@ -1,13 +1,13 @@
 -- Migration: acuranzo_1000.lua
 -- Bootstraps the migration system by creating the queries table and populating it with the next migration.
 
+-- luacheck: no max line length
+-- luacheck: no unused args
+
 -- CHANGELOG
 -- 2.0.0 - 2025-10-18 - Moved to latest migration format
 -- 1.1.0 - 2025-09-28 - Changed diagram query to use JSON table definition instead of PlantUML for custom ERD tool.
 -- 1.0.0 - 2025-09-13 - Initial creation for queries table with SQLite, PostgreSQL, MySQL, DB2 support.
-
--- luacheck: no max line length
--- luacheck: no unused args
 
 return function(engine, design_name, schema_name, cfg)
 local queries = {}
@@ -26,12 +26,7 @@ table.insert(queries,{sql=[[
         name                    ${VARCHAR_100}      NOT NULL,
         summary                 ${TEXTBIG}                  ,
         collection              ${JSON}                     ,
-        valid_after             ${TIMESTAMP_TZ}             ,
-        valid_until             ${TIMESTAMP_TZ}             ,
-        created_id              ${INTEGER}          NOT NULL,
-        created_at              ${TIMESTAMP_TZ}     NOT NULL,
-        updated_id              ${INTEGER}          NOT NULL,
-        updated_at              ${TIMESTAMP_TZ}     NOT NULL,
+        ${COMMON_CREATE}
         ${PRIMARY}(query_id),                                               -- Primary Key
         ${UNIQUE}(query_ref, query_type_a28)                             -- Unique Column
     );
@@ -72,12 +67,7 @@ table.insert(queries,{sql=[[
                 name                    ${VARCHAR_100}      NOT NULL,
                 summary                 ${TEXTBIG}                  ,
                 collection              ${JSON}                     ,
-                valid_after             ${TIMESTAMP_TZ}             ,
-                valid_until             ${TIMESTAMP_TZ}             ,
-                created_id              ${INTEGER}          NOT NULL,
-                created_at              ${TIMESTAMP_TZ}     NOT NULL,
-                updated_id              ${INTEGER}          NOT NULL,
-                updated_at              ${TIMESTAMP_TZ}     NOT NULL,
+                ${COMMON_CREATE}
                 ${PRIMARY}(query_id),                                       -- Primary Key
                 ${UNIQUE}(query_ref)                                        -- Unique Column
             );
@@ -94,7 +84,7 @@ table.insert(queries,{sql=[[
         ]=],
                                                                             -- summary
         NULL,                                                               -- collection
-        ${QUERIES_COMMON}
+        ${COMMON_INSERT}
     );
 
 ]]})
@@ -125,7 +115,7 @@ table.insert(queries,{sql=[[
         ]=],
                                                                             -- summary
         NULL,                                                               -- collection
-        ${QUERIES_COMMON}
+        ${COMMON_INSERT}
     );
 
 ]]})
@@ -278,54 +268,7 @@ table.insert(queries,{sql=[[
                                 "unique": false,
                                 "standard": true
                             },
-                            {
-                                "name": "valid_after",
-                                "datatype": "${TIMESTAMP_TZ}",
-                                "nullable": true,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            },
-                            {
-                                "name": "valid_until",
-                                "datatype": "${TIMESTAMP_TZ}",
-                                "nullable": true,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            },
-                            {
-                                "name": "created_id",
-                                "datatype": "${INTEGER}",
-                                "nullable": false,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            },
-                            {
-                                "name": "created_at",
-                                "datatype": "${TIMESTAMP_TZ}",
-                                "nullable": false,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            },
-                            {
-                                "name": "updated_id",
-                                "datatype": "${INTEGER}",
-                                "nullable": false,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            },
-                            {
-                                "name": "updated_at",
-                                "datatype": "${TIMESTAMP_TZ}",
-                                "nullable": false,
-                                "primary_key": false,
-                                "unique": false,
-                                "standard": true
-                            }
+                            ${COMMON_DIAGRAM}
                         ]
                     }
                 ]
@@ -334,7 +277,7 @@ table.insert(queries,{sql=[[
         ${JSON_INGEST_END}
                                                                             -- DIAGRAM_END
         ,                                                                   -- collection
-        ${QUERIES_COMMON}
+        ${COMMON_INSERT}
     );
 
 ]]})
