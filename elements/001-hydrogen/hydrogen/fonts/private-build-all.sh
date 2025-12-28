0ed696f3589c2bec0225c3a5c5cf62062f7aa14b
Andrew Simard <andrew@500foods.com>
2025-12-27 16:40:17 -0800

    More font-related fiddling.

--- elements/001-hydrogen/hydrogen/fonts/private-build-all.sh
+++ elements/001-hydrogen/hydrogen/fonts/private-build-all.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+rm -rf dist/ .verda-cache/ node_modules/.cache/ .build/
+npm cache clean --force
+
 npm run build -- --jCmd=8 woff2::AcuranzoSans
-nm run build -- --jCmd=8 woff2::AcuranzoSansFancy
+npm run build -- --jCmd=8 woff2::AcuranzoSansFancy
 npm run build -- --jCmd=8 woff2::AcuranzoMono 
 npm run build -- --jCmd=8 woff2::AcuranzoMonoFancy
 npm run build -- --jCmd=8 woff2-unhinted::AcuranzoSansMail
