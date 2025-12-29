ba647a1794b684302314af67eb0f28cc98687719
Andrew Simard <41052272+500Foods@users.noreply.github.com>
2025-12-29 01:27:24 -0800

    Update private-build-all.sh with build notes
    Added notes regarding build process and resource usage.

--- elements/023-vanadium/private-build-all.sh
+++ elements/023-vanadium/private-build-all.sh
@@ -3,6 +3,10 @@
 rm -rf dist/ .verda-cache/ node_modules/.cache/ .build/
 npm cache clean --force
 
+# Note: TTF files will be built as a pre-requisite for the WOFF2 files
+# Note: jCmd=8 will limit the build to 8 cores and also limit memory usage
+# Note: This is a very CPU and RAM-intensive build, taking potentially hours
+
 npm run build -- --jCmd=8 woff2::VanadiumSans
 npm run build -- --jCmd=8 woff2::VanadiumSansFancy
 npm run build -- --jCmd=8 woff2::VanadiumMono 
