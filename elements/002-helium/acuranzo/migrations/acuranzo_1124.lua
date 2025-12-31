e05997385660ca5795b3353ebbae90e054abd199
Andrew Simard <andrew@500foods.com>
2025-12-31 14:46:24 -0800

    Another pile of queries

---
+++ elements/002-helium/acuranzo/migrations/acuranzo_1124.lua
@@ -0,0 +1,192 @@
+-- Migration: acuranzo_1124.lua
+-- QueryRef #033 - Get Session Logs List + Search
+
+-- luacheck: no max line length
+-- luacheck: no unused args
+
+-- CHANGELOG
+-- 1.0.0 - 2025-12-31 - Initial creation
+
+return function(engine, design_name, schema_name, cfg)
+local queries = {}
+
+cfg.TABLE = "queries"
+cfg.MIGRATION = "1124"
+cfg.QUERY_REF = "033"
+cfg.QUERY_NAME = "Get Session Logs List + Search"
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+table.insert(queries,{sql=[[
+
+    INSERT INTO ${SCHEMA}${QUERIES} (
+        ${QUERIES_INSERT}
+    )
+    WITH next_query_id AS (
+        SELECT COALESCE(MAX(query_id), 0) + 1 AS new_query_id
+        FROM ${SCHEMA}${QUERIES}
+    )
+    SELECT
+        new_query_id                                                        AS query_id,
+        ${MIGRATION}                                                        AS query_ref,
+        ${STATUS_ACTIVE}                                                    AS query_status_a27,
+        ${TYPE_FORWARD_MIGRATION}                                           AS query_type_a28,
+        ${DIALECT}                                                          AS query_dialect_a30,
+        ${QTC_SLOW}                                                         AS query_queue_a58,
+        ${TIMEOUT}                                                          AS query_timeout,
+        [=[
+            INSERT INTO ${SCHEMA}${QUERIES} (
+                ${QUERIES_INSERT}
+            )
+            WITH next_query_id AS (
+                SELECT COALESCE(MAX(query_id), 0) + 1 AS new_query_id
+                FROM ${SCHEMA}${QUERIES}
+            )
+            SELECT
+                new_query_id                                                        AS query_id,
+                ${QUERY_REF}                                                        AS query_ref,
+                ${STATUS_ACTIVE}                                                    AS query_status_a27,
+                ${TYPE_SQL}                                                         AS query_type_a28,
+                ${DIALECT}                                                          AS query_dialect_a30,
+                ${QTC_MEDIUM}                                                       AS query_queue_a58,
+                ${TIMEOUT}                                                          AS query_timeout,
+                [==[
+                    SELECT
+                        session_id,
+                        account_id,
+                        session_length,
+                        session_issues,
+                        session_changes,
+                        session_secs,
+                        session_mins,
+                        status_a25,
+                        flag_a26,
+                        sessions.created_at,
+                        sessions.updated_at,
+                        ${JRS}lua25.collection${JRM}'$.icon'${JRE}) status_icon
+                    FROM (
+                        SELECT
+                            session_id,
+                            account_id,
+                            SUM(session_length) session_length,
+                            SUM(session_issues) session_issues,
+                            SUM(session_changes) session_changes,
+                            MAX(session_secs) session_secs,
+                            MAX(status_lua_25) status_lua_25,
+                            MIN(flag_lua_26) flag_lua_26,
+                            MIN(created_at) created_at,
+                            MAX(updated_at) updated_at,
+                            1 + (SUM(session_secs) / 60) session_mins
+                        FROM
+                            ${SCHEMA}sessions
+                        WHERE
+                            (account_id = :ACCOUNTID)
+                        GROUP BY
+                            session_id,
+                            account_id
+                    ) sessions
+                    LEFT OUTER JOIN
+                        ${SCHEMA}lookups lua25
+                        ON lua25.lookup_id = 25
+                        AND lua25.key_idx = sessions.status_lua_25
+                    WHERE
+                        session_id IN (
+                            SELECT
+                                session_id
+                            FROM
+                                ${SCHEMA}sessions
+                            WHERE
+                                (account_id = :ACCOUNTID)
+                                AND (
+                                    (UPPER(session_id) LIKE '%' || :SEARCH || '%')
+                                    OR (UPPER(session) LIKE '%' || :SEARCH || '%')
+                                )
+                        )
+                    ORDER BY
+                        sessions.created_at DESC
+                ]==]                                                                AS code,
+                '${QUERY_NAME}'                                                     AS name,
+                [==[
+                    #  QueryRef #${QUERY_REF} - ${QUERY_NAME}
+
+
+                    ## Parameters
+
+
+                    ## Returns
+
+
+                    ## Tables
+
+
+                    ## Notes
+
+                ]==]
+                                                                                    AS summary,
+                '{}'                                                                AS collection,
+                ${COMMON_INSERT}
+            FROM next_query_id;
+
+            ${SUBQUERY_DELIMITER}
+
+            UPDATE ${SCHEMA}${QUERIES}
+              SET query_type_a28 = ${TYPE_APPLIED_MIGRATION}
+            WHERE query_ref = ${MIGRATION}
+              and query_type_a28 = ${TYPE_FORWARD_MIGRATION};
+        ]=]
+                                                                            AS code,
+        'Populate QueryRef #${QUERY_REF} - ${QUERY_NAME}'                   AS name,
+        [=[
+            # Forward Migration ${MIGRATION}: Poulate QueryRef #${QUERY_REF} - ${QUERY_NAME}
+
+            This migration creates the query for QueryRef #${QUERY_REF} - ${QUERY_NAME}
+        ]=]
+                                                                            AS summary,
+        '{}'                                                                AS collection,
+        ${COMMON_INSERT}
+    FROM next_query_id;
+
+]]})
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+table.insert(queries,{sql=[[
+
+    INSERT INTO ${SCHEMA}${QUERIES} (
+        ${QUERIES_INSERT}
+    )
+    WITH next_query_id AS (
+        SELECT COALESCE(MAX(query_id), 0) + 1 AS new_query_id
+        FROM ${SCHEMA}${QUERIES}
+    )
+    SELECT
+        new_query_id                                                        AS query_id,
+        ${MIGRATION}                                                        AS query_ref,
+        ${STATUS_ACTIVE}                                                    AS query_status_a27,
+        ${TYPE_REVERSE_MIGRATION}                                           AS query_type_a28,
+        ${DIALECT}                                                          AS query_dialect_a30,
+        ${QTC_SLOW}                                                         AS query_queue_a58,
+        ${TIMEOUT}                                                          AS query_timeout,
+        [=[
+            DELETE FROM ${SCHEMA}${TABLE}
+            WHERE query_ref = ${QUERY_REF};
+
+            ${SUBQUERY_DELIMITER}
+
+            UPDATE ${SCHEMA}${QUERIES}
+              SET query_type_a28 = ${TYPE_FORWARD_MIGRATION}
+            WHERE query_ref = ${MIGRATION}
+              and query_type_a28 = ${TYPE_APPLIED_MIGRATION};
+        ]=]
+                                                                            AS code,
+        'Remove QueryRef #${QUERY_REF} - ${QUERY_NAME}'                     AS name,
+        [=[
+            # Reverse Migration ${MIGRATION}: Remove QueryRef #${QUERY_REF} - ${QUERY_NAME}
+
+            This is provided for completeness when testing the migration system
+            to ensure that forward and reverse migrations are complete.
+        ]=]
+                                                                            AS summary,
+        '{}'                                                                AS collection,
+        ${COMMON_INSERT}
+    FROM next_query_id;
+
+]]})
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+return queries end
---
+++ elements/002-helium/acuranzo/migrations/acuranzo_1126.lua
@@ -0,0 +1,172 @@
+-- Migration: acuranzo_1126.lua
+-- QueryRef #035 - Get Lookup Key
+
+-- luacheck: no max line length
+-- luacheck: no unused args
+
+-- CHANGELOG
+-- 1.0.0 - 2025-12-31 - Initial creation
+
+return function(engine, design_name, schema_name, cfg)
+local queries = {}
+
+cfg.TABLE = "queries"
+cfg.MIGRATION = "1126"
+cfg.QUERY_REF = "035"
+cfg.QUERY_NAME = "Get Lookup Key"
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+table.insert(queries,{sql=[[
+
+    INSERT INTO ${SCHEMA}${QUERIES} (
+        ${QUERIES_INSERT}
+    )
+    WITH next_query_id AS (
+        SELECT COALESCE(MAX(query_id), 0) + 1 AS new_query_id
+        FROM ${SCHEMA}${QUERIES}
+    )
+    SELECT
+        new_query_id                                                        AS query_id,
+        ${MIGRATION}                                                        AS query_ref,
+        ${STATUS_ACTIVE}                                                    AS query_status_a27,
+        ${TYPE_FORWARD_MIGRATION}                                           AS query_type_a28,
+        ${DIALECT}                                                          AS query_dialect_a30,
+        ${QTC_SLOW}                                                         AS query_queue_a58,
+        ${TIMEOUT}                                                          AS query_timeout,
+        [=[
+            INSERT INTO ${SCHEMA}${QUERIES} (
+                ${QUERIES_INSERT}
+            )
+            WITH next_query_id AS (
+                SELECT COALESCE(MAX(query_id), 0) + 1 AS new_query_id
+                FROM ${SCHEMA}${QUERIES}
+            )
+            SELECT
+                new_query_id                                                        AS query_id,
+                ${QUERY_REF}                                                        AS query_ref,
+                ${STATUS_ACTIVE}                                                    AS query_status_a27,
+                ${TYPE_SQL}                                                         AS query_type_a28,
+                ${DIALECT}                                                          AS query_dialect_a30,
+                ${QTC_MEDIUM}                                                       AS query_queue_a58,
+                ${TIMEOUT}                                                          AS query_timeout,
+                [==[
+                    SELECT
+                        AL.lookup_id,
+                        AL.key_idx,
+                        AL.value_txt,
+                        AL.value_int,
+                        AL.sort_seq,
+                        AL.status_a1,
+                        AL.summary,
+                        AL.code,
+                        AL.collection,
+                        AL.valid_after,
+                        AL.valid_until,
+                        AL.created_id,
+                        AL.created_at,
+                        AL.updated_id,
+                        AL.updated_at,
+                        (${SIZE_INTEGER} * 7)
+                        + (${SIZE_TIMESTAMP} * 4)
+                        + COALESCE(LENGTH(AL.value_txt), 0)
+                        + COALESCE(LENGTH(AL.summary), 0)
+                        + COALESCE(LENGTH(AL.code), 0)
+                        + ${SIZE_COLLECTION}
+                        record_size
+                    FROM
+                        ${SCHEMA}lookups AL
+                    WHERE
+                        (AL.lookup_id = :LOOKUPID)
+                        and (AL.key_idx = :KEYIDX)
+                    ORDER BY
+                        AL.sort_seq,
+                        AL.key_idx,
+                        AL.value_txt,
+                        AL.value_int
+                ]==]                                                                AS code,
+                '${QUERY_NAME}'                                                     AS name,
+                [==[
+                    #  QueryRef #${QUERY_REF} - ${QUERY_NAME}
+
+
+                    ## Parameters
+
+
+                    ## Returns
+
+
+                    ## Tables
+
+
+                    ## Notes
+
+                ]==]
+                                                                                    AS summary,
+                '{}'                                                                AS collection,
+                ${COMMON_INSERT}
+            FROM next_query_id;
+
+            ${SUBQUERY_DELIMITER}
+
+            UPDATE ${SCHEMA}${QUERIES}
+              SET query_type_a28 = ${TYPE_APPLIED_MIGRATION}
+            WHERE query_ref = ${MIGRATION}
+              and query_type_a28 = ${TYPE_FORWARD_MIGRATION};
+        ]=]
+                                                                            AS code,
+        'Populate QueryRef #${QUERY_REF} - ${QUERY_NAME}'                   AS name,
+        [=[
+            # Forward Migration ${MIGRATION}: Poulate QueryRef #${QUERY_REF} - ${QUERY_NAME}
+
+            This migration creates the query for QueryRef #${QUERY_REF} - ${QUERY_NAME}
+        ]=]
+                                                                            AS summary,
+        '{}'                                                                AS collection,
+        ${COMMON_INSERT}
+    FROM next_query_id;
+
+]]})
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+table.insert(queries,{sql=[[
+
+    INSERT INTO ${SCHEMA}${QUERIES} (
+        ${QUERIES_INSERT}
+    )
+    WITH next_query_id AS (
+        SELECT COALESCE(MAX(query_id), 0) + 1 AS new_query_id
+        FROM ${SCHEMA}${QUERIES}
+    )
+    SELECT
+        new_query_id                                                        AS query_id,
+        ${MIGRATION}                                                        AS query_ref,
+        ${STATUS_ACTIVE}                                                    AS query_status_a27,
+        ${TYPE_REVERSE_MIGRATION}                                           AS query_type_a28,
+        ${DIALECT}                                                          AS query_dialect_a30,
+        ${QTC_SLOW}                                                         AS query_queue_a58,
+        ${TIMEOUT}                                                          AS query_timeout,
+        [=[
+            DELETE FROM ${SCHEMA}${TABLE}
+            WHERE query_ref = ${QUERY_REF};
+
+            ${SUBQUERY_DELIMITER}
+
+            UPDATE ${SCHEMA}${QUERIES}
+              SET query_type_a28 = ${TYPE_FORWARD_MIGRATION}
+            WHERE query_ref = ${MIGRATION}
+              and query_type_a28 = ${TYPE_APPLIED_MIGRATION};
+        ]=]
+                                                                            AS code,
+        'Remove QueryRef #${QUERY_REF} - ${QUERY_NAME}'                     AS name,
+        [=[
+            # Reverse Migration ${MIGRATION}: Remove QueryRef #${QUERY_REF} - ${QUERY_NAME}
+
+            This is provided for completeness when testing the migration system
+            to ensure that forward and reverse migrations are complete.
+        ]=]
+                                                                            AS summary,
+        '{}'                                                                AS collection,
+        ${COMMON_INSERT}
+    FROM next_query_id;
+
+]]})
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+return queries end
