f727e3b4594d68fd2c9c8a0e3e86cfafa3c17de2
Andrew Simard <andrew@500foods.com>
2026-03-05 11:30:51 -0800

    More work on Lithium

---
+++ elements/003-lithium/eslint.config.js
@@ -0,0 +1,51 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+      globals: {
+        // Browser globals
+        window: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly',
+        localStorage: 'readonly',
+        sessionStorage: 'readonly',
+        fetch: 'readonly',
+        console: 'readonly',
+        CustomEvent: 'readonly',
+        EventTarget: 'readonly',
+        HTMLElement: 'readonly',
+        URL: 'readonly',
+        Intl: 'readonly',
+        atob: 'readonly',
+        btoa: 'readonly',
+        setTimeout: 'readonly',
+        clearTimeout: 'readonly',
+        setInterval: 'readonly',
+        clearInterval: 'readonly',
+        Response: 'readonly',
+        Headers: 'readonly',
+        indexedDB: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'prefer-const': 'error',
+      'no-var': 'error',
+    },
+  },
+  {
+    ignores: [
+      'node_modules/',
+      'dist/',
+      'coverage/',
+      'tests/lib/',
+      'tests/*.sh',
+      'src/init/',
+    ],
+  },
+];
