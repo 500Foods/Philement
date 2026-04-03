911e2949bdd4bdaf81b76535305400c6a9989016
Andrew Simard <andrew@500foods.com>
2026-04-03 16:12:00 -0700

    Lithium - saving and loading templates is kind of working

--- elements/003-lithium/public/service-worker.js
+++ elements/003-lithium/public/service-worker.js
@@ -1,7 +1,7 @@
 // Lithium PWA Service Worker
 // Cache strategy: cache-first for statics, stale-while-revalidate for API data
 
-const CACHE_VERSION = 1974;
+const CACHE_VERSION = 1984;
 const STATIC_CACHE = `lithium-static-v${CACHE_VERSION}`;
 const API_CACHE = `lithium-api-v${CACHE_VERSION}`;
 
